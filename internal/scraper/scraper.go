272fe9a8e3baaf76e9e334deff70fd49735aafe5
toni <nickschtino@gmail.com>
2025-12-03 17:50:37 +0100

    removed debug code

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,13 +1,9 @@
 package scraper
 
 import (
-	"os"
-	"strings"
-
 	"github.com/gocolly/colly"
 )
 
-// Scrape TODO: currently overwrites every result it finds -> only return last result
 func Scrape(url string, sel string) (string, error) {
 	c := colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"),
@@ -18,12 +14,6 @@ func Scrape(url string, sel string) (string, error) {
 		result = e.Text
 	})
 
-	c.OnResponse(func(r *colly.Response) {
-		// debug purposes
-		if strings.Contains(r.Request.URL.String(), "mediamarkt") {
-			os.WriteFile("debug_mediamarkt.html", r.Body, 0644)
-		}
-	})
 	err := c.Visit(url)
 	if err != nil {
 		return "", err
